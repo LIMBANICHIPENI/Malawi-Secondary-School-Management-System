ba344eb6fc57c14185cceb6c4b08b2846322184a
LIMBANICHIPENI <bed-com-13-21@unima.ac.mw>
2026-01-05 10:13:50 +0100

    branch

--- msse/src/App.js
+++ msse/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+/* Teacher Dashboard */
+import TeacherDashboard from "./Components/TeacherDashboard/TeacherDashboard";
+import Home from "./Components/TeacherDashboard/Home";
+import Lessons from "./Components/TeacherDashboard/Lessons";
+import StudentsList from "./Components/TeacherDashboard/StudentsList";
+import TeacherCollaboration from "./Components/TeacherDashboard/teacherCollaboration";
+import Grades from "./Components/TeacherDashboard/Grades";
+import Assignments from "./Components/TeacherDashboard/Assignments";
+import Resources from "./Components/TeacherDashboard/Resources";
+import Announcements from "./Components/TeacherDashboard/annoucements";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+
+      {/* Teacher Dashboard Routes */}
+      <Route path="/" element={<TeacherDashboard />}>
+        <Route index element={<Home />} />
+        <Route path="lessons" element={<Lessons />} />
+        <Route path="students" element={<StudentsList />} />
+        <Route path="collaboration" element={<TeacherCollaboration />} />
+        <Route path="grades" element={<Grades />} />
+        <Route path="assignments" element={<Assignments />} />
+        <Route path="resources" element={<Resources />} />
+        <Route path="announcements" element={<Announcements />} />
+      </Route>
+
+    </Routes>
   );
 }
 
