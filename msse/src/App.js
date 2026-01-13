364737aecf7c395e47ee37f641e502eb15e99142
taonanyasulu@gmail.com <taonanyasulu@gmail.com>
2026-01-13 07:30:50 +0200

    My commits

--- msse/src/App.js
+++ msse/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Layouts from './Layouts/Layouts.jsx';  // Import Layouts instead
-import './Components/TeacherDashboard/TeacherDashboard.css'; // Keep CSS import if needed
+import StudentDashboard from './Components/StudentDashboard/StudentDashboard';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Layouts />  // Render Layouts
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div className="App">
+      <StudentDashboard />
+    </div>
+  );
+}
+
+export default App;
